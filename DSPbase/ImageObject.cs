066ae4c250eecf34ae46018f5e55e5bd1a0e28ab
Lipotam <Lipotam@gmial.com>
2013-02-25 00:08:46 +0300

    ImageObject.cs added

---
+++ DSPbase/ImageObject.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace DSPbase
+{
+    class ImageObject
+    {
+        private List<Point> pointList;
+        private int perimeter;
+
+        public int Area
+        {
+            get
+            {
+                return pointList.Count;
+            }
+        }
+
+        public int Perimeter
+        {
+            get
+            {
+                return perimeter;
+            }
+        }
+
+        public double Elongation
+        {
+            get
+            {
+                int m02 = GetMoment(0, 2),
+                    m20 = GetMoment(2, 0),
+                    m11 = GetMoment(1, 1);
+                double sqrtRezult = Math.Sqrt((m20 - m02) * (m20 - m02) + 4 * m11 * m11);
+
+                return (m20 + m02 + sqrtRezult) / (m20 + m02 - sqrtRezult);
+            }
+        }
+
+        public Point MassCenter
+        {
+            get
+            {
+                return new Point(
+                       (int)pointList.Average(t => t.X),
+                       (int)pointList.Average(t => t.Y));
+            }
+        }
+
+        public double Density
+        {
+            get
+            {
+                return (double)perimeter * perimeter / Area;
+            }
+        }
+
+        public ImageObject()
+        {
+            pointList = new List<Point>();
+            perimeter = 0;
+        }
+
+        public void AddPoint(Point objectPoint)
+        {
+            pointList.Add(objectPoint);
+        }
+
+
+        public void IncrementPerimeter()
+        {
+            perimeter++;
+        }
+        private int GetMoment(int i, int j)
+        {
+            int result = 0;
+            Point massCenter = MassCenter;
+
+            foreach (Point point in pointList)
+            {
+                result += power(point.X - massCenter.X, i) * power(point.Y - massCenter.Y, j);
+            }
+            return result;
+        }
+
+        private int power(int x, int power)
+        {
+            if (power == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (power == 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    return x * x;
+                }
+            }
+        }
+    }
+}
