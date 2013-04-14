5d10f79b67d3c0c527b73d423d3b2bf51a78a3ea
Lipotam <Lipotam@gmial.com>
2013-04-14 19:34:27 +0300

    lab 10 interface made

--- DSP_lab10/Program.cs
+++ DSP_lab10/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GoodRBF
+namespace DSP_lab10
 {
     static class Program
     {
@@ -16,7 +15,7 @@ namespace GoodRBF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GoodRBF());
+            Application.Run(new Form1());
         }
     }
 }
